f7869de2e8fad2bc63f89bc16f87ca58d1fcce31
gammer <gammer@DESKTOP-P9A700P>
2021-02-14 12:25:19 -0600

    Agregando referencias

---
+++ TuSuper/TuSuper/TuSuper.BL/ProductosBL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuSuper.BL
+{
+    public class ProductosBL
+    {
+        public List<Producto> ObtenerProducutos()
+        {
+            var producto1 = new Producto();
+            producto1.Id = 1;
+            producto1.Descripcion = "Arroz";
+            producto1.Precio = 150;
+
+            var producto2 = new Producto();
+            producto2.Id = 2;
+            producto2.Descripcion = "Manzana";
+            producto2.Precio = 25;
+
+            var producto3 = new Producto();
+            producto3.Id = 3;
+            producto3.Descripcion = "Coca Cola";
+            producto3.Precio = 60;
+
+
+            var listadeProductos = new List<Producto>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+
+          
+            return listadeProductos;
+        }
+            
+    }
+}
--- TuSuper/TuSuper/TuSuper.Web/Controllers/ProductoController.cs
+++ TuSuper/TuSuper/TuSuper.Web/Controllers/ProductoController.cs
@@ -3,33 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using TuSuper.Web.Models;
+using TuSuper.BL;
 
 namespace TuSuper.Web.Controllers
 {
     public class ProductoController : Controller
     {
-        // GET: Producto
+        // GET: Productos
         public ActionResult Index()
         {
-
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Arroz";
-
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Manzana";
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 3;
-            producto3.Descripcion = "Coca Cola";
-
-
-            var listadeProductos = new List<ProductoModel>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProducutos();
 
             return View(listadeProductos);
         }
