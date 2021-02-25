c6c202f87cf21bf9d283fd59790f3b4ef74c1f6a
gammer <gammer@DESKTOP-P9A700P>
2021-02-24 19:14:56 -0600

    Agregar archivos de proyecto.

---
+++ TuSuper.Web/Controllers/ProductosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TuSuper.BL;
+
+namespace TuSuper.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProducutos();
+
+            return View(listadeProductos);
+        }
+    }
+}
